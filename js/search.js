a8d1501c7be2e72daf8b98e16654f66d57c9acc3
Lowry Nakamura <lowry.nakamura@gmail.com>
2019-04-26 21:00:01 -0700

    added notes about jquery code

--- js/search.js
+++ js/search.js
@@ -1,4 +1,6 @@
+//this function is registering an event listener for the document load
 $(function () {
+	//this function is targeting the searchbar and registering a keyup event listener
 	$('#searchBar').keyup(function () {
 		var searchText = $( this ).val().toLowerCase();
 		if (searchText === '') {
